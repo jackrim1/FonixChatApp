af087a522c5cdc1309acfb92be8b8ca0fc374e12
jackrim1 <jriminton@gmail.com>
2021-12-13 14:56:56 +0000

    message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_11_171322) do
+ActiveRecord::Schema.define(version: 2021_12_13_100142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2021_12_11_171322) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_memberships_on_room_id"
     t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -43,4 +53,6 @@ ActiveRecord::Schema.define(version: 2021_12_11_171322) do
 
   add_foreign_key "memberships", "rooms"
   add_foreign_key "memberships", "users"
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
